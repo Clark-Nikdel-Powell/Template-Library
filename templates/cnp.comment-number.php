e30eb93500c677d0eea94251b72a397fe9c2ecae
Joe Cruz <joe@clarknikdelpowell.com>
2016-11-09 20:20:52 -0500

    small fixes

--- templates/cnp.comment-number.php
+++ templates/cnp.comment-number.php
@@ -5,7 +5,7 @@ class CommentNumber extends Organism {
 	public function __construct( $name = 'comment-number', $tag = 'div', $attributes = [], $content = '', $data = null, $before = '', $prepend = '', $append = '', $after = '' ) {
 		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
 
-		if ( ! empty( $this->data ) && isset( $this->data->ID ) ) {
+		if ( isset( $this->data ) && isset( $this->data->ID ) ) {
 			$this->content = get_comments_number( $this->data->ID );
 		}
 	}
--- templates/cnp.image.php
+++ templates/cnp.image.php
@@ -4,15 +4,26 @@ namespace CNP;
 
 class Image extends Organism {
 
-	public $attachment_id;
+	private $attachment_id = '';
 	public $image_size;
 	public $icon;
 
-	public function __construct( $attachment_id, $image_size, $icon, $name = 'image', $tag = 'div', $attributes = [], $content = '', $data = null, $before = '', $prepend = '', $append = '', $after = '' ) {
+	public function __construct( $image_size, $icon, $name = 'image', $tag = '', $attributes = [], $content = '', $data = null, $before = '', $prepend = '', $append = '', $after = '' ) {
 		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
 
-		$this->attachment_id = $attachment_id;
-		$this->image_size    = $image_size;
-		$this->icon          = $icon;
+		$this->image_size = $image_size;
+		$this->icon       = $icon;
+
+		if ( isset( $data ) && is_object( $data ) ) {
+			$this->attachment_id = $data->ID;
+		} elseif ( isset( $data ) && is_array( $data ) ) {
+			$this->attachment_id = $data['ID'];
+		} else {
+			$this->attachment_id = $data;
+		}
+	}
+
+	public function get_markup() {
+		return wp_get_attachment_image( $this->attachment_id, $this->image_size, $this->icon, $this->attributes );
 	}
 }
