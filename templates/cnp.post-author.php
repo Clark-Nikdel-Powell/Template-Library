f272d09ce3cd899131361e3a4c117aa794140ce0
Glenn Welser <glenn.welser@gmail.com>
2016-11-15 22:57:13 -0500

    simplify post-author

--- templates/cnp.post-author.php
+++ templates/cnp.post-author.php
@@ -3,20 +3,10 @@ namespace CNP;
 
 class PostAuthor extends Organism {
 
-	private $author = '';
-
 	public function __construct( $data, $author_meta = 'display_name', $name = 'post-author', $tag = 'p', array $attributes, $content = '', $before = '', $prepend = 'By: ', $append = '', $after = '' ) {
 
 		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
 
-		if ( isset( $data ) && is_object( $data ) ) {
-			$this->author = $data->post_author;
-		} elseif ( isset( $data ) && is_array( $data ) ) {
-			$this->author = $data['post_author'];
-		} else {
-			$this->author = $data;
-		}
-
-		$this->content = get_the_author_meta( $author_meta, $this->author );
+		$this->content = get_the_author_meta( $author_meta, $data->post_author );
 	}
 }
