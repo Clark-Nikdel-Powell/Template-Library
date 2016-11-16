06b5f910b8d455d79f746090b97be8d35f7e221a
Glenn Welser <glenn.welser@gmail.com>
2016-11-15 21:58:29 -0500

    add date format to post-date org

--- templates/cnp.post-date.php
+++ templates/cnp.post-date.php
@@ -3,10 +3,24 @@ namespace CNP;
 
 class PostDate extends Organism {
 
-	public function __construct( $data, $name = 'postdate', $tag = 'p', array $attributes, $content = '', $before = '', $prepend = '<strong>Published:</strong> ', $append = '', $after = '' ) {
+	public $date_format;
 
-		$date_format = get_option( 'date_format' );
+	public function __construct( $data, $date_format = '', $name = 'post-date', $tag = 'p', array $attributes, $content = '', $before = '', $prepend = '<strong>Published:</strong> ', $append = '', $after = '' ) {
 
-		parent::__construct( $name, $tag, $attributes, get_the_date( $date_format, $data ), $data, $before, $prepend, $append, $after );
+		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
+
+		$this->set_date_format( $date_format );
+		$this->content = get_the_date( $date_format, $data );
+	}
+
+	private function set_date_format( $date_format ) {
+
+		if ( '' !== $date_format ) {
+			$this->date_format = $date_format;
+
+			return;
+		}
+
+		$this->date_format = get_option( 'date_format' );
 	}
 }
