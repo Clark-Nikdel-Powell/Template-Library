90b3d66c81b14dd976056e4e381c44aca85019c8
Glenn Welser <glenn.welser@gmail.com>
2016-11-15 22:21:13 -0500

    add post author org to postheaderarchive

--- templates/cnp.post-author.php
+++ templates/cnp.post-author.php
@@ -3,10 +3,20 @@ namespace CNP;
 
 class PostAuthor extends Organism {
 
+	private $author = '';
+
 	public function __construct( $data, $author_meta = 'display_name', $name = 'post-author', $tag = 'p', array $attributes, $content = '', $before = '', $prepend = 'By: ', $append = '', $after = '' ) {
 
 		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
 
-		$this->content = get_the_author_meta( $author_meta, $data );
+		if ( isset( $data ) && is_object( $data ) ) {
+			$this->author = $data->post_author;
+		} elseif ( isset( $data ) && is_array( $data ) ) {
+			$this->author = $data['post_author'];
+		} else {
+			$this->author = $data;
+		}
+
+		$this->content = get_the_author_meta( $author_meta, $this->author );
 	}
 }
--- templates/cnp.post-header-archive.php
+++ templates/cnp.post-header-archive.php
@@ -3,21 +3,24 @@ namespace CNP;
 
 class PostHeaderArchive extends Organism {
 
-	public $posttitle;
-	public $postdate;
+	public $post_title;
+	public $post_date;
+	public $post_author;
 
 	public function __construct( $data, $name = 'postheaderarchive', $tag = 'div', array $attributes, $content = '', $before = '', $prepend = '', $append = '', $after = '' ) {
 
 		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
 
-		$this->posttitle = new PostTitleLink( $data, $name . '__title' );
-		$this->postdate  = new PostDate( $data, $name . '__postdate' );
+		$this->post_title  = new PostTitleLink( $data, $name . '__title' );
+		$this->post_date   = new PostDate( $data, $name . '__date' );
+		$this->post_author = new PostAuthor( $data, '', $name . '__author' );
 	}
 
 	public function get_markup() {
 
-		$this->content = $this->posttitle->get_markup();
-		$this->content .= $this->postdate->get_markup();
+		$this->content = $this->post_title->get_markup();
+		$this->content .= $this->post_date->get_markup();
+		$this->content .= $this->post_author->get_markup();
 
 		return parent::get_markup();
 	}
