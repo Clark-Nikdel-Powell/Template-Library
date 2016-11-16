c52c63cc5f22378af8a8a3764095d74266d4ad04
Glenn Welser <glenn.welser@gmail.com>
2016-11-15 22:19:29 -0500

    change the way date_format is passed in

--- templates/cnp.post-date.php
+++ templates/cnp.post-date.php
@@ -3,24 +3,18 @@ namespace CNP;
 
 class PostDate extends Organism {
 
-	public $date_format;
+	private $date_format = '';
 
 	public function __construct( $data, $date_format = '', $name = 'post-date', $tag = 'p', array $attributes, $content = '', $before = '', $prepend = '<strong>Published:</strong> ', $append = '', $after = '' ) {
 
 		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
-		$this->set_date_format( $date_format );
 
-		$this->content = get_the_date( $date_format, $data );
-	}
-
-	private function set_date_format( $date_format ) {
-
-		if ( '' !== $date_format ) {
+		if ( empty( $date_format ) ) {
+			$this->date_format = get_option( 'date_format' );
+		} else {
 			$this->date_format = $date_format;
-
-			return;
 		}
 
-		$this->date_format = get_option( 'date_format' );
+		$this->content = get_the_date( $this->date_format, $data );
 	}
 }
