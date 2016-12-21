20ca7c44256eac88e029dde7ac2a7eb6472e4211
Josh Nederveld <joshnederveld@gmail.com>
2016-12-21 16:47:18 -0500

    Added SchemaAddress, fixes #16

---
+++ src/SchemaAddress.php
@@ -0,0 +1,52 @@
+<?php
+namespace CNP\TemplateLibrary;
+
+/**
+ * Class SchemaAddress
+ * @package CNP\TemplateLibrary
+ */
+class SchemaAddress extends Organism {
+
+	public $address_data;
+
+	public function __construct( $address_data, $name = 'schema-address', $tag = 'div', array $attributes = [], $before = '', $prepend = '', $append = '', $after = '' ) {
+
+		parent::__construct( $name, $tag, $attributes, $content, $data, $structure, $before, $prepend, $append, $after );
+
+		$this->address_data = $address_data;
+
+		$address_pieces = array();
+
+		if ( isset( $this->address_data['street_address'] ) && ! empty( $this->address_data['street_address'] ) ) {
+			$address_pieces[] = '<span itemprop="streetAddress">' . $this->address_data['street_address'] . '</span>';
+		}
+
+		if ( isset( $this->address_data['city'] ) && ! empty( $this->address_data['city'] ) ) {
+			$address_pieces[] = '<span itemprop="addressLocality">' . $this->address_data['city'] . '</span>';
+		}
+
+		if ( isset( $this->address_data['state'] ) && ! empty( $this->address_data['state'] ) ) {
+			$address_pieces[] = ', <span itemprop="addressRegion">' . $this->address_data['state'] . '</span>';
+		}
+
+		if ( isset( $this->address_data['zip_code'] ) && ! empty( $this->address_data['zip_code'] ) ) {
+			$address_pieces[] = ' <span itemprop="postalCode">' . $this->address_data['zip_code'] . '</span>';
+		}
+
+		if ( isset( $this->address_data['country'] ) && ! empty( $this->address_data['country'] ) ) {
+			$address_pieces[] = ', <span itemprop="addressCountry">' . $this->address_data['country'] . '</span>';
+		}
+
+		$address = '<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">' . implode( '', $address_pieces ) . '</div>';
+
+		$this->content = $address;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_markup() {
+
+		return parent::get_markup();
+	}
+}
