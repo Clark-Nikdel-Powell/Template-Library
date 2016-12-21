4d6f6d972b72236d725d8e3f03e096641404916a
Josh Nederveld <joshnederveld@gmail.com>
2016-12-21 11:05:56 -0500

    Added extra parameters, documentation

--- src/Container.php
+++ src/Container.php
@@ -1,13 +1,31 @@
 <?php
 namespace CNP\TemplateLibrary;
 
+/**
+ * Class Container
+ * @package CNP\TemplateLibrary
+ */
 class Container extends Organism {
 
-	public function __construct( $name, array $attributes ) {
+	/**
+	 * Container constructor.
+	 *
+	 * @param string $name
+	 * @param array $attributes
+	 * @param array $structure
+	 * @param string $before
+	 * @param null $prepend
+	 * @param array $append
+	 * @param string $after
+	 */
+	public function __construct( $name, array $attributes, $structure, $before, $prepend, $append, $after ) {
 
-		parent::__construct( $name, 'div', $attributes );
+		parent::__construct( $name, 'div', $attributes, null, null, $structure, $before, $prepend, $append, $after );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function get_markup() {
 
 		return parent::get_markup();
