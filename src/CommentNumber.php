234c107a04c5cf8e50a0f663990b6103228f185b
Josh Nederveld <joshnederveld@gmail.com>
2016-12-21 11:01:55 -0500

    Cleanup/docs

--- src/CommentNumber.php
+++ src/CommentNumber.php
@@ -1,17 +1,37 @@
 <?php
 namespace CNP\TemplateLibrary;
 
+/**
+ * Class CommentNumber
+ * @package CNP\TemplateLibrary
+ *
+ * @link https://developer.wordpress.org/reference/functions/get_comments_number/
+ */
 class CommentNumber extends Organism {
 
-	public function __construct( $name = 'comment-number', $tag = 'div', $attributes = [], $content = '', $data = null, $before = '', $prepend = '', $append = '', $after = '' ) {
+	/**
+	 * CommentNumber constructor.
+	 *
+	 * @param string $name
+	 * @param int|WP_Post $data Optional. Post ID or WP_Post object. Default is global $post.
+	 * @param string $tag
+	 * @param array $attributes
+	 * @param string $content
+	 * @param string $before
+	 * @param string $prepend
+	 * @param string $append
+	 * @param string $after
+	 */
+	public function __construct( $name = 'comment-number', $data = 0, $tag = 'div', $attributes = [], $content = '', $before = '', $prepend = '', $append = '', $after = '' ) {
 
-		parent::__construct( $name, $tag, $attributes, $content, $data, $before, $prepend, $append, $after );
+		parent::__construct( $name, $tag, $attributes, $content, $data, null, $before, $prepend, $append, $after );
 
-		if ( isset( $this->data ) && isset( $this->data->ID ) ) {
-			$this->content = get_comments_number( $this->data->ID );
-		}
+		$this->content = get_comments_number( $this->data );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function get_markup() {
 
 		return parent::get_markup();
