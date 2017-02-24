333d8ef5f694dd0ffb2c741975a3307c95f9db49
Josh Nederveld <joshnederveld@gmail.com>
2017-02-24 15:49:35 -0500

    Cleanup

--- src/Organism.php
+++ src/Organism.php
@@ -3,42 +3,119 @@ namespace CNP\TemplateLibrary;
 
 /**
  * Class Organism
+ *
  * @package CNP\TemplateLibrary
  *
- * @link https://developer.wordpress.org/reference/functions/apply_filters/
+ * @link    https://developer.wordpress.org/reference/functions/apply_filters/
  */
 abstract class Organism {
 
+	/**
+	 * The Organism name
+	 *
+	 * @var string $name
+	 */
 	public $name;
-	public $tag;
-	public $attributes;
-	// TODO: ask again about attribute quote style.
-	public $content;
+
+	/**
+	 * Data, mixed
+	 *
+	 * @var string $data
+	 */
 	public $data;
+
+	/**
+	 * Content
+	 *
+	 * @var string $content
+	 */
+	public $content;
+
+	/**
+	 * The Organism name
+	 *
+	 * @var string $name
+	 */
+	public $tag;
+
+	/**
+	 * Attributes
+	 *
+	 * @var string $attributes
+	 */
+	public $attributes;
+
+	/**
+	 * Structure
+	 *
+	 * @var array $structure
+	 */
 	public $structure;
+
+	/**
+	 * Parent Name
+	 *
+	 * @var string $parent_name
+	 */
 	public $parent_name;
+
+	/**
+	 * Separator
+	 *
+	 * @var string $separator
+	 */
 	public $separator;
+
+	/**
+	 * Before
+	 *
+	 * @var string $before
+	 */
 	public $before;
+
+	/**
+	 * Prepend
+	 *
+	 * @var string $prepend
+	 */
 	public $prepend;
+
+	/**
+	 * Append
+	 *
+	 * @var string $append
+	 */
 	public $append;
+
+	/**
+	 * After
+	 *
+	 * @var string $after
+	 */
 	public $after;
+
+	/**
+	 * Hide
+	 *
+	 * @var bool $hide
+	 */
 	public $hide = false;
 
 	/**
 	 * Organism constructor.
 	 *
-	 * @param string $name
-	 * @param string $tag
-	 * @param array $attributes
-	 * @param string $content
-	 * @param null $data
-	 * @param array $structure
-	 * @param string $parent_name
-	 * @param string $separator
-	 * @param string $before
-	 * @param string $prepend
-	 * @param string $append
-	 * @param string $after
+	 * @param string $name        The Organism name.
+	 * @param null   $data        Data, mixed.
+	 * @param string $content     Content.
+	 * @param string $tag         The tag.
+	 * @param array  $attributes  Attributes.
+	 * @param array  $structure   A structure array.
+	 * @param string $parent_name The parent name to scope children on.
+	 * @param string $separator   The separator between parent and children names.
+	 * @param string $before      Markup/text before the tag.
+	 * @param string $prepend     Markup/text before the content.
+	 * @param string $append      Markup/text after the content.
+	 * @param string $after       Markup/text after the tag.
 	 */
 	public function __construct( $name = '', $data = null, $content = '', $tag = 'div', array $attributes = [], array $structure = [], $parent_name = '', $separator = '__', $before = '', $prepend = '', $append = '', $after = '' ) {
 
@@ -64,8 +141,7 @@ abstract class Organism {
 	 *
 	 * @return string
 	 */
-	public
-	function get_markup() {
+	public function get_markup() {
 
 		// Note: If a child organism overwrites get_markup, please include Organism->do_filter so that we don't have a filterless Organism.
 		$this->do_filter();
@@ -83,12 +159,9 @@ abstract class Organism {
 	 * Run a namespaced filter for this organism, if we're in WordPress.
 	 * The standard practice is to modify the object itself.
 	 *
-	 * @param string $suffix
+	 * @param string $suffix The suffix to put on the filter.
 	 */
-	public
-	function do_filter(
-		$suffix = ''
-	) {
+	public function do_filter( $suffix = '' ) {
 
 		if ( defined( 'WP_CONTENT_DIR' ) ) {
 			$filter_name = $this->name . $suffix;
@@ -101,8 +174,7 @@ abstract class Organism {
 	 *
 	 * @return string
 	 */
-	public
-	function get_attributes() {
+	public function get_attributes() {
 
 		// Add class for the Organism name.
 		if ( key_exists( 'class', $this->attributes ) ) {
@@ -135,23 +207,21 @@ abstract class Organism {
 	}
 
 	/**
-	 * get_content
+	 * Gets the content
 	 *
 	 * @return string
 	 */
-	public
-	function get_content() {
+	public function get_content() {
 
 		return $this->prepend . $this->content . $this->get_structure() . $this->append;
 	}
 
 	/**
-	 * get_structure
+	 * Gets the structure of an Organism
 	 *
 	 * @return string
 	 */
-	public
-	function get_structure() {
+	public function get_structure() {
 
 		if ( ! isset( $this->structure ) || ! is_array( $this->structure ) ) {
 			return '';
@@ -167,7 +237,7 @@ abstract class Organism {
 				$child->parent_name = $this->parent_name;
 
 				// Prepend the parent name to the child name.
-				$child->name        = $child->parent_name . $this->separator . $child->name;
+				$child->name = $child->parent_name . $this->separator . $child->name;
 			}
 
 			$structure .= $child->get_markup();
@@ -177,56 +247,43 @@ abstract class Organism {
 	}
 
 	/**
-	 * class_name
-	 *
 	 * Returns the current organism's class name.
 	 *
 	 * @return string
 	 */
-	public
-	function class_name() {
+	public function class_name() {
 
 		return strtolower( get_class( $this ) );
 	}
 
 	/**
-	 * class_root
+	 * Generates the class root.
 	 *
 	 * @return string
 	 */
-	public
-	function class_root() {
+	public function class_root() {
 
 		return sprintf( '%s_%s_', $this->class_name(), $this->name );
 	}
 
 	/**
-	 * organism_name
-	 *
 	 * Returns a namespaced organism based on the current name and the given org_name.
 	 * Example: TODO: add example
 	 *
-	 * @param $org_name
-	 * @param $separator
+	 * @param string $org_name  The organism name.
+	 * @param string $separator The separator.
 	 *
 	 * @return string
 	 */
-	public
-	function organism_name(
-		$org_name, $separator = '__'
-	) {
+	public function organism_name( $org_name, $separator = '__' ) {
 
 		return $this->name . $separator . $org_name;
 	}
 
 	/**
-	 * debug
-	 *
 	 * Used for debugging purposes.
 	 */
-	public
-	function debug() {
-
+	public function debug() {
 		var_dump( $this );
 	}
 }
