e4b7581e1b0e6d58bf47e99b63922cb8250d8d6d
Josh Nederveld <joshnederveld@gmail.com>
2017-02-24 13:14:35 -0500

    Class fix

--- src/PostContent.php
+++ src/PostContent.php
@@ -1,18 +1,34 @@
 <?php
 namespace CNP\TemplateLibrary;
 
+/**
+ * Class PostContent
+ *
+ * @package CNP\TemplateLibrary
+ */
 class PostContent extends Content {
 
+	/**
+	 * A CSS class.
+	 *
+	 * @var string $class
+	 */
 	public $class;
 
+	/**
+	 * PostContent constructor.
+	 *
+	 * @param string $name The organism name.
+	 * @param string $class A CSS class.
+	 */
 	public function __construct( $name = 'postcontent', $class = 'postcontent' ) {
 
 		parent::__construct( $name, get_the_content() );
-		array_push( $this->attributes['class'], $this->class );
-	}
 
-	public function get_markup() {
-
-		return parent::get_markup();
+		if ( is_array( $this->attributes['class'] ) ) {
+			array_push( $this->attributes['class'], $this->class );
+		} else {
+			$this->attributes['class'] = $this->class;
+		}
 	}
 }
