fae8957f69b7ca3488ff9e4bdc18133d9d1794db
Josh Nederveld <joshnederveld@gmail.com>
2017-03-08 16:46:24 -0500

    Cleaned up

--- src/LinkPost.php
+++ src/LinkPost.php
@@ -3,37 +3,30 @@ namespace CNP\TemplateLibrary;
 
 /**
  * Class LinkPost
+ *
  * @package CNP\TemplateLibrary
  *
  * A post link with customizable content. To display a post link with the post title, use PostTitleLink instead.
  *
- * @see Link
- * @link https://developer.wordpress.org/reference/functions/get_permalink/
+ * @see     Link
+ * @link    https://developer.wordpress.org/reference/functions/get_permalink/
  */
 class LinkPost extends Link {
 
 	/**
 	 * PostLink constructor.
 	 *
-	 * @param string $content Required. The content for the link.
-	 * @param string $name Optional. Defaults to 'postlink'.
-	 * @param array $attributes
-	 * @param int|WP_Post $data Optional. Takes either a post ID or a WP_Post object. Defaults to the global $post.
-	 * @param string $before
-	 * @param string $prepend
-	 * @param string $append
-	 * @param string $after
+	 * @param string       $name       Optional. Defaults to 'postlink'.
+	 * @param string       $content    Required. The content for the link.
+	 * @param array        $attributes Other attributes.
+	 * @param int|\WP_Post $data       Optional. Takes either a post ID or a WP_Post object. Defaults to the global $post.
+	 * @param string       $before     Before opening tag.
+	 * @param string       $prepend    Inside opening tag.
+	 * @param string       $append     Before closing tag.
+	 * @param string       $after      After closing tag.
 	 */
 	public function __construct( $name = 'link-post', $content, array $attributes = [], $data = null, $before = '', $prepend = '', $append = '', $after = '' ) {
 
-		parent::__construct( $href = get_permalink( $data ), $content, $name, $attributes, $before, $prepend, $append, $after );
-	}
-
-	/**
-	 * @return string
-	 */
-	public function get_markup() {
-
-		return parent::get_markup();
+		parent::__construct( $name, $href = get_permalink( $data ), $content, $attributes, $before, $prepend, $append, $after );
 	}
 }
