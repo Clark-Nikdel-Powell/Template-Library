3fdd1a645ecad332e276385f462c89b7704fe4e7
Josh Nederveld <joshnederveld@gmail.com>
2017-03-13 16:36:10 -0400

    Refactored out optional args

--- src/TaxonomyList.php
+++ src/TaxonomyList.php
@@ -3,54 +3,53 @@ namespace CNP\TemplateLibrary;
 
 /**
  * Class TaxonomyList
+ *
  * @package CNP\TemplateLibrary
  *
- * @link https://developer.wordpress.org/reference/functions/get_post/
- * @link https://developer.wordpress.org/reference/functions/get_object_taxonomies/
- * @link https://developer.wordpress.org/reference/functions/get_the_terms/
+ * @link    https://developer.wordpress.org/reference/functions/get_post/
+ * @link    https://developer.wordpress.org/reference/functions/get_object_taxonomies/
+ * @link    https://developer.wordpress.org/reference/functions/get_the_terms/
  */
 class TaxonomyList extends Organism {
 
+	/**
+	 * Registered taxonomy name.
+	 *
+	 * @var string
+	 */
 	public $taxonomy;
+
+	/**
+	 * Characters to put in between taxonomy items.
+	 *
+	 * @var string
+	 */
 	public $delimiter;
 
 	/**
 	 * TaxonomyList constructor.
 	 *
-	 * @param null $taxonomy
-	 * @param string $delimiter
-	 * @param string|array|WP_post $data Optional. Name of the type of taxonomy object, or an object (row from posts). Defaults to global $post.
-	 * @param string $name
-	 * @param string $tag
-	 * @param array $attributes
-	 * @param string $before
-	 * @param string $prepend
-	 * @param string $append
-	 * @param string $after
+	 * @param string $name      Organism name.
+	 * @param string $taxonomy  Registered taxonomy name.
+	 * @param string $delimiter Inserted between each item.
 	 */
-	public function __construct( $name = 'taxonomy-list', $taxonomy = null, $delimiter = ', ', $data = null, $tag = 'div', array $attributes = [], $before = '', $prepend = '', $append = '', $after = '' ) {
+	public function __construct( $name = 'taxonomy-list', $taxonomy = '', $delimiter = ', ' ) {
 
-		parent::__construct( $name, $data, $content = '', $tag, $attributes, $structure = [], $parent_name = '', $separator = '__', $before, $prepend, $append, $after );
+		parent::__construct( $name, $data = null, $content = '', $tag = 'div', $attributes = [], $structure = [], $parent_name = '', $separator = '__', $before = '', $prepend = '', $append = '', $after = '' );
 
-		// This catches if we didn't pass anything in.
-		if ( null === $this->data ) {
-			$this->data = get_post();
-		}
-
+		$this->data      = get_post();
 		$this->delimiter = $delimiter;
 
 		if ( null !== $taxonomy ) {
 			$this->taxonomy = $taxonomy;
 		} else {
 			$post_object_taxonomies = get_object_taxonomies( $this->data );
-			$this->taxonomy = array_shift( $post_object_taxonomies );
+			$this->taxonomy         = array_shift( $post_object_taxonomies );
 		}
 	}
 
 	/**
-	 * get_content
-	 *
-	 * @return string
+	 * Gets the list.
 	 */
 	public function get_content() {
 
@@ -69,13 +68,5 @@ class TaxonomyList extends Organism {
 		} else {
 			$this->content = '';
 		}
-	}
-
-	/**
-	 * @return string
-	 */
-	public function get_markup() {
-
-		return parent::get_markup();
 	}
 }
