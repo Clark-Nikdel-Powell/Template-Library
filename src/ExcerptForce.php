73812c868f2834647650208662c494d7091c4308
Josh Nederveld <joshnederveld@gmail.com>
2017-03-13 16:21:30 -0400

    Args adjust

--- src/ExcerptForce.php
+++ src/ExcerptForce.php
@@ -7,7 +7,7 @@ namespace CNP\TemplateLibrary;
  *
  * Forces an excerpt by using get_the_excerpt
  *
- * @link https://developer.wordpress.org/reference/functions/get_the_excerpt/
+ * @link    https://developer.wordpress.org/reference/functions/get_the_excerpt/
  */
 class ExcerptForce extends Excerpt {
 
@@ -17,13 +17,13 @@ class ExcerptForce extends Excerpt {
 	 * @param string $name
 	 * @param string $data Optional. WP_Post Object. Set by Excerpt if not defined.
 	 * @param string $tag
-	 * @param array $attributes
+	 * @param array  $attributes
 	 * @param string $before
 	 * @param string $prepend
 	 * @param string $append
 	 * @param string $after
 	 */
-	public function __construct( $name = 'excerpt', $data, $tag = 'p', array $attributes = [], $before = '', $prepend = '', $append = '', $after = '' ) {
+	public function __construct( $name = 'excerpt', $data = null, $tag = 'p', array $attributes = [], $before = '', $prepend = '', $append = '', $after = '' ) {
 
 		parent::__construct( $name, $tag, $attributes, $data, $before, $prepend, $append, $after );
 
